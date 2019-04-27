57ae0b2de457a3a31cb056e0b30bc683bd43c5a1
cckn <cckn1985@gmail.com>
2019-04-27 18:34:50 +0900

    Using api to get data from the server

--- src/App.js
+++ src/App.js
@@ -2,49 +2,38 @@ import React, { Component } from 'react'
 
 import './App.css'
 import Movie from './Movie'
-import Video from './Video'
 
 class App extends Component {
   state = {}
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: 'matrix',
-            poster:
-              'https://previews.123rf.com/images/ismagilov/ismagilov1502/ismagilov150201922/36895784-%EB%B8%94%EB%A3%A8-%EB%A7%A4%ED%8A%B8%EB%A6%AD%EC%8A%A4-%ED%81%B0-%EA%B0%80%EC%83%81-%ED%99%94%EB%A9%B4.jpg',
-          },
-          {
-            title: 'old boy',
-            poster:
-              'https://upload.wikimedia.org/wikipedia/ko/thumb/4/48/Old_Boy.jpg/250px-Old_Boy.jpg',
-          },
-          {
-            title: 'i see the evil',
-            poster:
-              'http://image.cine21.com/resize/cine21/poster/2010/0804/M0010007__1.jpg',
-          },
-          {
-            title: 'star wars',
-            poster:
-              'https://upload.wikimedia.org/wikipedia/ko/thumb/6/6a/%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg/250px-%EC%8A%A4%ED%83%80%EC%9B%8C%EC%A6%88_%EA%B9%A8%EC%96%B4%EB%82%9C_%ED%8F%AC%EC%8A%A4.jpg',
-          },
-          {
-            title: 'load of the ling',
-            poster:
-              'http://image.chosun.com/sitedata/image/201711/14/2017111401718_0.jpg',
-          },
-        ],
-      })
-    }, 2000)
+    this._getMovies()
   }
 
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    console.log(movies[0])
+    this.setState({ movies })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+      .then(responcse => responcse.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err))
+  }
   _renderMovies = () => {
     {
-      const movies = this.state.movies.map((movie, index) => {
-        return <Movie title={movie.title} poster={movie.poster} key={index} />
+      const movies = this.state.movies.map(movie => {
+        return (
+          <Movie
+            title={movie.title}
+            poster={movie.medium_cover_image}
+            key={movie.id}
+            genres={movie.genres}
+            synopsis={movie.synopsis}
+          />
+        )
       })
       return movies
     }
@@ -53,7 +42,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Video />
         {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     )
--- src/Movie.js
+++ src/Movie.js
@@ -2,24 +2,46 @@ import React from 'react'
 import './Movie.css'
 import PropTypes from 'prop-types'
 
-function Movie({ title, poster }) {
+function Movie({ title, poster, genres, synopsis }) {
   return (
-    <div>
-      <MoviePoster poster={poster} />
-      <h1>{title}</h1>
+    <div className="Movie">
+      <div className="Movie__Columns">
+        <MoviePoster alt={title} poster={poster}/>
+      </div>
+      <div className="Movie__Columns">
+        <h1>{title}</h1>
+        <div className="Movie__genres">
+          {genres.map((genre, index) => (
+            <MovieGenre genre={genre} key={index}/>
+          ))}
+        </div>
+        <p className="Movie__Synopsis">{synopsis}</p>
+      </div>
     </div>
   )
 }
+
+function MoviePoster({ alt, poster }) {
+  return <img alt={alt} src={poster} className="Movie__Poster"/>
+}
+
+function MovieGenre({ genre }) {
+  return <span className="Movie__Genre">{genre} </span>
+}
 Movie.propTypes = {
-    title: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.array.isRequired,
+  synopsis: PropTypes.string.isRequired,
+}
+
+MoviePoster.propTypes = {
+  alt: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
 }
 
-function MoviePoster({ poster }) {
-  return <img alt="" src={poster} />
-}
-MoviePoster.propTypes = {
-  poster: PropTypes.string.isRequired,
+MovieGenre.propTypes = {
+  genre: PropTypes.string.isRequired,
 }
 
 export default Movie
